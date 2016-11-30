89971c63fef841bfe7ecea41d0a11b06c9f112be
Jenny Lai <jenny.lai@flatironschool.com>
2016-11-30 11:22:25 -0500

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,34 @@
+## code your solution here.
+# In this lab, you'll be creating a Cat class. Every instance of a Cat should have a name and be able to meow.
+#In other words, I should be able to do:
+# maru = Cat.new
+# maru.name = "Maru"
+#
+# maru.name
+# # => "Maru"
+#
+# maru.meow
+# # "meow!"
+# # => nil
+# Instructions
+# Run the test suite to get started. You'll be writing all your code in the lib/meowing_cat.rb file.
+# Define a class, called Cat.
+# Use the attr_accessor macro to create a setter and getter method for a cat's name.
+# Write a method, .meow, that outputs "meow!" to the terminal using the puts method when called on an
+#instance of Cat.
+
+class Cat
+
+  attr_accessor :name
+
+  def meow
+    puts "meow!"
+  end
+
+end
+
+maru = Cat.new
+maru.name = "Maru"
+puts maru.name
+puts maru.meow
 ## code your solution here. 
